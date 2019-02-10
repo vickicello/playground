c766c25f5e3ce93418fabe00c06f4ed5968d045f
vickicello <vicki.cello@gmail.com>
2019-02-10 11:36:22 -0500

    add file permissions

--- javascript.js
+++ javascript.js
@@ -1,12 +1,14 @@
 // add event listener using target
 
-document.addEventListener('DOMContentLoaded', function() {
-  let app = document.getElementById('todo-app')
+// document.addEventListener('DOMContentLoaded', function() {
+//   let app = document.getElementById('todo-app')
 
-  app.addEventListener('click', function(e){
-    if (e.target && e.target.nodeName === 'LI' ) {
-      let item = e.target;
-      alert('you clicked an item' + item.innerHTML)
-    }
-  })
-})
+//   app.addEventListener('click', function(e){
+//     if (e.target && e.target.nodeName === 'LI' ) {
+//       let item = e.target;
+//       alert('you clicked an item' + item.innerHTML)
+//     }
+//   })
+// })
+
+console.log('hey!');
