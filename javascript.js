158e1e4db4c89e792d3a663df0716e49b7fc21e8
vickicello <vicki.cello@gmail.com>
2019-02-11 19:06:32 -0500

    finish follow up tasks

--- javascript.js
+++ javascript.js
@@ -1,17 +1,17 @@
-document.addEventListener('click', myFunction)
+document.addEventListener('click', myFunction);
   function myFunction(){
-    counter = 0;
-    // alert('hello!');
+    counter = 2;
+
     for (i=0; i < 300; i++) {
-      var repeatedText = document.getElementById('border')
-      var newParagraph = document.createTextNode('Hello World \n');
+      var repeatedText = document.getElementById("border");
+      var para = document.createElement("P");  
+      var newParagraph = document.createTextNode('Hello, World \n');
         if (counter % 5 == 0) { 
           newParagraph = document.createTextNode('Hello, CoverMyMeds \n');
         }
-        repeatedText.appendChild(newParagraph);
-      // document.createTextNode('hello world').appendChild(repeatedText);
+        
+      para.appendChild(newParagraph);
+      document.body.appendChild(para);
       counter ++;
-     
     }
   }
-
