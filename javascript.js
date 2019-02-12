2630e9ddfda20a555d3bec37b5128b2352bce8e1
vickicello <vicki.cello@gmail.com>
2019-02-11 19:30:05 -0500

    clean up code and fix counter

--- javascript.js
+++ javascript.js
@@ -2,16 +2,17 @@ document.addEventListener('click', myFunction);
   function myFunction(){
     counter = 2;
 
-    for (i=0; i < 300; i++) {
+    for (i=1; i < 300; i++) {
       var repeatedText = document.getElementById("border");
       var para = document.createElement("P");  
       var newParagraph = document.createTextNode('Hello, World \n');
         if (counter % 5 == 0) { 
           newParagraph = document.createTextNode('Hello, CoverMyMeds \n');
         }
-        
+
       para.appendChild(newParagraph);
       document.body.appendChild(para);
       counter ++;
     }
   }
+
--- playground.rb
+++ playground.rb
@@ -5,11 +5,9 @@ my_text =  File.read("/Users/victoriahamm/Desktop/playground/words.txt")
   def count_words(my_text)
   only_chars =  my_text.delete('.,"?/0-9/').split
 
-  # only_chars.delete_if {|w| w.is_a? Integer }
   puts only_chars.group_by {|w| w.downcase }
     .map {|k, v| [k, v.count]}
     .sort_by {|k, v| [v, k]}.reverse
   end
 
 puts count_words(my_text)
-
